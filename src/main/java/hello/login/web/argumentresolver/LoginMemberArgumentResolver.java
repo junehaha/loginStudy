f8324ac2cf124884d8076cc38897972834556812
Juneha Hwang <skdiwnsgk99@gmail.com>
2024-02-26 22:52:51 +0900

    A

--- src/main/java/hello/login/web/argumentresolver/LoginMemberArgumentResolver.java
+++ src/main/java/hello/login/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -26,5 +26,6 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
             return null;
         }
         return session.getAttribute(SessionConst.LOGIN_MEMBER);
+
     }
 }
