50565a39d876a0597e3e1c4f2f43c0bb7e0de1de
Juneha Hwang <skdiwnsgk99@gmail.com>
2024-04-07 20:39:01 +0900

    a

--- src/main/java/hello/login/web/argumentresolver/LoginMemberArgumentResolver.java
+++ src/main/java/hello/login/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -22,9 +22,6 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
 
 
 
-
-
-
     @Override
     public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) nativeWebRequest.getNativeRequest();
